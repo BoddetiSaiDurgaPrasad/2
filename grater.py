ac4df1802377589780137f900cca0118b60212c8
BoddetiSaiDurgaPrasad <94205751+BoddetiSaiDurgaPrasad@users.noreply.github.com>
2021-11-20 21:54:04 +0530

    Add files via upload

---
+++ grater.py
@@ -0,0 +1,85 @@
+####a=int(input('enter a number'))
+####b=int(input('enter b value'))
+####if (a>b or a==b):
+####    print('a is bigger')
+####else:
+####    print('a and b are same')
+####    
+
+##i=11
+##while (i>=0):
+##    print(i)
+##    i-=1
+
+##i=1
+##while (i<=11):
+##    print(i)
+##    i+=1
+
+##break
+##for i in range (1,11):
+##    if i==5:
+##        break
+##    print(i,end=' ')
+##    
+
+##continue
+##for i in range (1,11):
+##    if i==5:
+##        continue
+##    print(i,end='')
+####sir program    
+##for i in range(1,10,2):
+##    print(i,end=' ')
+
+##for i in range(1,6):
+##    print(2,'*',i,'=',2*i)
+
+##n=int(input('enter a number'))
+##r=int(input('entr starting point'))
+##e=int(input('enter ending point'))
+##for i in range(r,e-1,-1):
+##    print(n,'*',i,'=',n*i)
+
+##n=int(input("entetr a number"))
+##i=1
+##while i<=10:
+##    print(n,'*',i,'=',n*i)
+##    i+=1
+
+##n=int(input('enter a number'))
+##s=int(input('starting point'))
+##e=int(input('ending point'))    
+##for i in range(s,e+1):
+##    if(i%n==0):
+##        print(i,end='  ')
+
+n=int(input('enter a number'))
+s=int(input('starting point'))
+
+c=0
+for i in range(1,s):
+    if (n/i==0):
+        c+
+if(c==2):
+    print('prime number')
+else:
+    print('not a prime number')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
